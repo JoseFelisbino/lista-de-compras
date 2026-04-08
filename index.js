c34011df03a745ca039ca1850c2c2ac04317fbad
JoseFelisbino <lu31660@gmail.com>
2026-04-07 23:51:40 -0300

    feat: adiciona lista

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const adicionarItem = document.getElementById('adicionar-item');
 adicionarItem.addEventListener("click", (evento) => {
     evento.preventDefault();
 
-    console.log(input.value);
+    if (input.value === "") {
+        alert("Por favor insira uma tarefa");
+        return;
+    }
+
+    const intemLista = document.createElement("li");
+    const containerIntemLista = document.createElement("div");
+    containerIntemLista.classList.add("lista-item-container");
 });
