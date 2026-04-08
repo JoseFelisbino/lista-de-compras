793434cd0e676298d9648ffb4892fe2e271aacf4
JoseFelisbino <lu31660@gmail.com>
2026-04-07 23:57:27 -0300

    feat: adiciona checkbox

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('input-item');
 
 const adicionarItem = document.getElementById('adicionar-item');
+let contador = 0;
 
 adicionarItem.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -13,4 +14,7 @@ adicionarItem.addEventListener("click", (evento) => {
     const intemLista = document.createElement("li");
     const containerIntemLista = document.createElement("div");
     containerIntemLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
 });
